88bbe39627580814858987e511e345f35cec72f1
Herqz <pilrealthegunner@gmail.com>
2025-09-15 22:46:54 +0700

    Commit No. 3 - Fix application crash when user input wrong color code [intent]

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -1,5 +1,8 @@
 package com.example.lab_week_02_b
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +15,7 @@ import androidx.core.graphics.toColorInt
 class ResultActivity : AppCompatActivity() {
     companion object{
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +23,21 @@ class ResultActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
 
-        if(intent != null){
+        if(intent != null) {
             val colorCode = intent.getStringExtra(COLOR_KEY)
 
             val backgroundScreen = findViewById<ConstraintLayout>(R.id.background_screen)
-            backgroundScreen.setBackgroundColor("#$colorCode".toColorInt())
+            try {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            } catch (ex: IllegalArgumentException) {
+                val errorIntent = Intent()
+                errorIntent.putExtra(ERROR_KEY, true)
+                setResult(Activity.RESULT_OK, errorIntent)
+                finish()
+            }
+
             val resultMessage = findViewById<TextView>(R.id.color_code_result_message)
-            resultMessage.text = getString(R.string.color_code_result_message, colorCode?.uppercase())
+            resultMessage.text = getString(R.string.color_code_result_message, colorCode)
         }
     }
 }
